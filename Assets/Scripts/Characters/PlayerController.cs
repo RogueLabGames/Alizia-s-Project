a471ae73ea9cb94b50b24645e50d15258440a4cb
DriveSP <gtavvice@hotmail.es>
2024-10-19 14:14:45 +0200

    Sword Animation Testing
    Sword Animation Testing

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -25,13 +25,15 @@ public class PlayerController : MonoBehaviour
         float horizontalMove = Input.GetAxisRaw("Horizontal"); //Horizontal move
         float verticalMove = Input.GetAxisRaw("Vertical"); //Vertical move
 
-        rig.velocity = new Vector2 (horizontalMove, verticalMove).normalized * speed; //This change attribute velocity of the RigidBody2D. Moves the character
+        rig.velocity = new Vector2(horizontalMove, verticalMove).normalized * speed; //This change attribute velocity of the RigidBody2D. Moves the character
         Animations(horizontalMove, verticalMove); //Call Animations method to control every animations
     }
 
     //Method that control all player's animations
     void Animations(float horizontalMove, float verticalMove)
     {
+
+        //Horizontal move
         if (horizontalMove > 0) //Right move
         {
             anim.SetBool("isWalkingH", true);
@@ -46,6 +48,31 @@ public class PlayerController : MonoBehaviour
         {
             anim.SetBool("isWalkingH", false);
         }
+
+
+        //Vertical move
+        if (verticalMove > 0)
+        {
+            anim.SetBool("isWalkingDown", false);
+            anim.SetBool("isWalkingUp", true);
+        }
+        else if (verticalMove < 0) //Left move
+        {
+            anim.SetBool("isWalkingUp", false);
+            anim.SetBool("isWalkingDown", true);
+        }
+        else //Idle
+        {
+            anim.SetBool("isWalkingDown", false);
+            anim.SetBool("isWalkingUp", false);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Attacking");
+            anim.SetTrigger("isAttacking");
+
+        }
+
     }
-    
 }
